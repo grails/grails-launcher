485881e20ecc4df378e94248b7b0325db0f6bbc0
Luke Daley <ld@ldaley.com>
2012-10-18 18:02:43 -0400

    Use reflection utils.

--- grails-launcher/src/main/java/org/grails/launcher/rootloader/RootLoaderFactory.java
+++ grails-launcher/src/main/java/org/grails/launcher/rootloader/RootLoaderFactory.java
@@ -17,12 +17,15 @@
 package org.grails.launcher.rootloader;
 
 import org.grails.launcher.context.GrailsLaunchContext;
+import org.grails.launcher.util.ReflectionUtils;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.grails.launcher.util.ReflectionUtils.invokeMethodWrapException;
 
 public class RootLoaderFactory {
 
@@ -43,7 +46,7 @@ public class RootLoaderFactory {
                 rootLoader.addURL(loggingBootstrapJar.toURI().toURL());
             }
             Class cls = rootLoader.loadClass("org.springframework.util.Log4jConfigurer");
-            invokeStaticMethod(cls, "initLogging", new Object[]{"classpath:grails-maven/log4j.properties"});
+            invokeMethodWrapException(cls, "initLogging", new Object[]{"classpath:grails-maven/log4j.properties"});
         }
 
         return rootLoader;
@@ -61,26 +64,6 @@ public class RootLoaderFactory {
         return urls.toArray(new URL[urls.size()]);
     }
 
-    /**
-     * Invokes the named method on a target object using reflection. The method signature is determined by the classes of each argument.
-     *
-     * @param target The object to call the method on.
-     * @param name The name of the method to call.
-     * @param args The arguments to pass to the method (may be an empty array).
-     * @return The value returned by the method.
-     */
-    private static Object invokeStaticMethod(Class target, String name, Object[] args) {
-        Class<?>[] argTypes = new Class[args.length];
-        for (int i = 0; i < args.length; i++) {
-            argTypes[i] = args[i].getClass();
-        }
-
-        try {
-            return target.getMethod(name, argTypes).invoke(target, args);
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-    }
 }
 
 
